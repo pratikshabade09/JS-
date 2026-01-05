6263620f5ca0bf49219f775ec5a42be572a7b978
pratikshabade09 <badepratiksha37@gmail.com>
2026-01-05 22:09:32 +0530

    upto lec 11

--- name.js
+++ name.js
@@ -65,3 +65,83 @@ let num2 = +"";
 // console.log(num1); //1 
 // console.log(num2); //0 --> bcz "" gives null which means 0 
 
+//video8
+//comparing datatypes
+// console.log(null > 0)   //false
+// console.log(null == 0)  //fale
+// console.log(null >= 0)  //true
+
+// console.log(undefined > 0)  //false
+// console.log(undefined == 0) //false
+// console.log(undefined >= 0) //false
+
+// console.log(undefined == null) //true (converts into comparable datatype and then compare)
+
+// console.log("2">=2)
+// console.log(1==true)
+//Avoide such comparisons bcz they are == and === are different here. 
+
+// console.log(undefined === null) //false (only compare when same datatype - stricter)
+
+//Use this instead of == for clean with datatype comparison
+
+
+//video9
+//datatypes summary
+//Types: Primitive(uses stack for memory) & Non-Primitive(uses heap for memory)
+
+//Primitive(by Value): String, Number, undefined, null, Boolean, Symbol(for uniqueness), BigInt
+
+//Non-Primitive(by Reference --> jinka memory mei reference diya jata hain): Objects, Arrays, Functions
+
+//NOTE: JS IS DYNAMICALLY TYPED LANGUAGE AND NOT STATICALLY TYPED LANGUAGE i.e. we don't need to define the datatype while declaring variable.
+
+const bigNumber = 23456789987654324567n //Here 'n' denotes that the declared number is of BigInt datatype and not a normal number.
+
+const score = 454
+const count = Symbol(89)
+const marks = Symbol(89)
+// for uniqueness --> Symbol  
+// console.log(count === marks) //they are unique due to 'Symbol'
+
+//All Non primitive DT has typeof 'object' eg., null --> obj, array --> obj, date --> object, map/set --> object, function --> function(called object function)
+
+//video10
+//stack and heap for memory
+
+//Stack --->Primitive DT (creates copy everytime)
+//Heap --> Non-Primitive DT (gives reference)
+let myID= 2342;
+let yourID = myID;  //In Stack, copy  of myID will be taken not original
+yourID = 34;
+// console.log(myID)
+// console.log(yourID)
+
+let obj = {
+    email: "mew@google.com",
+    contact: 98754587 
+}
+let obj2 = obj      //In heap, reference of object 'obj' not copy and value will change of obj's email.
+obj2.email = "sfv@google.mew"
+
+// console.log(obj.email)
+// console.log(obj2.email)
+
+
+//video11
+//concatenation: 
+let salary = 90
+let naam = "giraffee"
+// console.log(naam + " ki salary hain " + salary ) //old way
+
+// console.log(`${naam} ki salary hain ${salary}`) //modern and efficient way
+
+let subject1 ="Science"
+let subject = new String("Science") //through 'new' we can create a String object not a normal string. If done in inspect-->console, we can get various methods of strings.
+
+// console.log(subject)    //[String: 'Science']
+// console.log(subject1)   //Science
+
+//some methods: charAt(), replace(), includes(), trim(), slice(), split(), toUpperCase(), toLowerCase() --> check on mdm all others
+
+
